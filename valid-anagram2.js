1496b690f084e657cfcaa53adeedfdff4644eb7e
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-06-22 21:42:18 -0700

    in progress

---
+++ valid-anagram2.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+  //if lengths don't match, it can't be an anagram
+  if(s.length !== t.length){
+    return false;
+  }
+
+  //if they do match, we need to compare all letters
+  let myHashMap = {}
+
+  //iterate through the first word and create the Hashamp
+  for(let letter of s){
+    console.log(letter)
+    myHashMap[letter] = (0 | myHashMap[letter]) + 1
+    console.log(myHashMap)
+  }
+
+  //iterate through the second word and start deducting the count of the hashmap until we can't deduct or we reach the end
+  for(let letterTwo of t){
+    console.log(letterTwo)
+    if(myHashMap[letterTwo] == 0){
+      return false
+    }
+    myHashMap[letterTwo]--;
+    console.log(myHashMap)
+  }
+  
+
+  return true;
+};
+
+console.log(isAnagram('anagramZ', 'nagaramq'))
