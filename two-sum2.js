b52e7963dae6f0c5f964755d81c2a3593941ef8b
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-06-26 06:02:43 -0700

    solved

--- two-sum2.js
+++ two-sum2.js
@@ -3,15 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
+
+/*
+Runtime: 277 ms, faster than 8.69% of JavaScript online submissions for Two Sum.
+Memory Usage: 42.7 MB, less than 43.99% of JavaScript online submissions for Two Sum.
+*/
+
 var twoSum = function(nums, target) {
+//loop through all nums, and subtract the current num from target, and check if that result is present
+//somewhere else in nums using indexOf
 
     for(let i = 0; i < nums.length; i++){
-      if(nums.indexOf(target - nums[i]) !== -1 ){
-        console.log(nums.indexOf(target - nums[i]))
-        return [nums.indexOf(nums[i]),nums.indexOf(target - nums[i])];
+      if((nums.indexOf(target - nums[i]) !== -1) && (nums.indexOf(target - nums[i]) !== i) ){
+        //special case where both numbers are the same number
+        if(nums[i] === (target - nums[i])){
+          return [nums.indexOf(nums[i]),nums.indexOf(target - nums[i],i)];
+        }
+        //the rest of the cases
+        else{
+          return [nums.indexOf(nums[i]),nums.indexOf(target - nums[i],i + 1)];
+        }
+        
       }
     }
 }
 
-
-console.log(twoSum([3,3], 6))
+console.log(twoSum([3,3], 6));
