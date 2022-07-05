11f2ad6d5238c0d75a39b3b513fce70ff4e7d1ab
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-07-04 21:07:01 -0700

    solved, passing all tests

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -12,19 +12,26 @@ var maxProfit = function(prices) {
   let rightPointer = 1;
   let maxProfit = 0;
   //let pricesCopy = [...prices]
-  for(let i = 0; i < prices.length - 1; i++){
+  console.log(prices)
+  for(let i = 0; rightPointer < prices.length ; i++){
+    //first thing - calculate the profit based on the current positions 
     currentProfit = prices[rightPointer] - prices[leftPointer];
+    //next, decide if this profit is better than what we already have
     if(maxProfit < currentProfit){
-      maxProfit = currentProfit 
+      maxProfit = currentProfit
     }
-    console.log('buy:', prices[leftPointer], ' sell:', 
+    //print out the current state of affairs 
+    console.log('loop:', i, 'left:', prices[leftPointer], ' right:', 
                  prices[rightPointer], ' currentProfit:', currentProfit,
                  'maxProfit is: ', maxProfit) 
+    //now the algorithm goes here
     
+    //if right is less than left, r++, l++
     if(prices[rightPointer] < prices[leftPointer]){
+      leftPointer=rightPointer;
       rightPointer++;
-      leftPointer++;
     }
+    //else keep advancing right by itself
     else{
       rightPointer++;
     }
@@ -33,4 +40,4 @@ var maxProfit = function(prices) {
   return maxProfit;
 };
 
-console.log(maxProfit([2,1,2,1,0,1,2]));
+console.log(maxProfit([7,1,5,3,6,4]));
