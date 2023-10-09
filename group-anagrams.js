b6adb5ae9581d3cb7d6ea10a8eff692870f9b508
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2023-10-09 13:29:23 -0700

    Update group-anagrams.js
    Working solution

--- group-anagrams.js
+++ group-anagrams.js
@@ -1,5 +1,5 @@
 //Group Anagrams
-
+//https://leetcode.com/problems/group-anagrams
 
 /**
  * @param {string[]} strs
@@ -7,25 +7,47 @@
  */
 var groupAnagrams = function(strs) {
 
-    const sortedStrs = strs.map(item => item.split("").sort().join(""))
+    //Take strs, sort the individual phrases, then create and object with key value pairs
+    //Then sort that whole thing
+    //Then iterate through that, each time there is a change, create a new subarray
 
-    const mySet = new Set(sortedStrs);
-
-    const solutionArray =[];
-    mySet.forEach(item => {
-        const tempArray = []
-        strs.forEach(subItem => {
-            if(item === subItem.split("").sort().join("")){
-                tempArray.push(subItem)
-            }
-            console.log('inner loop')
-        })
-        console.log('outer loop')
-        solutionArray.push(tempArray)
+    const answerArray = [];
+    const sortedArray = strs.map(item => {
+        const myObject = {
+            mutated: '',
+            original: ''
+        }
+        myObject.original = item
+        myObject.mutated = item.split("").sort().join("");
+        return myObject
     })
-   return solutionArray
-   
 
+    const newArray = sortedArray.sort((a, b) => {
+        const nameA = a.mutated
+        const nameB = b.mutated
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        // names must be equal
+        return 0;
+    })
 
+    for(let index = 0; index < newArray.length; index++){
+        const subArray = [];
+        subArray.push(newArray[index].original)
+        while((index < newArray.length -1) && newArray[index].mutated === newArray[index+1].mutated){
+            subArray.push(newArray[index +1].original)
+            index++
+        }
+        answerArray.push(subArray);
+    }
+    return answerArray;
 }
     
+
+
+
+
