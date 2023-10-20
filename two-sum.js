3307e4c577ad42950cbfde59301acb2a96dba628
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2023-10-20 12:57:12 -0700

    Update two-sum.js
    Refactor with Linda!

--- two-sum.js
+++ two-sum.js
@@ -1,23 +1,21 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+//https://leetcode.com/problems/two-sum/submissions/
+
 var twoSum = function(nums, target) {
-    let originalNums = [...nums];
-    let sortedArr = nums.sort(function(a, b){return a-b});
-    console.log('sortedArr is:', sortedArr)
-    for(let i = 0; i < sortedArr.length; i++){
-        for(let j = 1; j < sortedArr.length; j++){
-            console.log('sortedArr[i]:', sortedArr[i], 'sortedArr[j]:', sortedArr[j])
-            if(sortedArr[i] + sortedArr[j] == target && i !== j){
-              console.log('i is:', i, 'j is:', j)
-              return [originalNums.indexOf(sortedArr[i]),originalNums.indexOf(sortedArr[j])]
-            }
-        }
+  
+  //create a new map object
+  let myMap = new Map();
+  
+  //iterate over the map object
+  for(let i = 0; i < nums.length; i++){  
+    //use the has method to see if the current number has a match
+    let complement = target - nums[i]
+    if(myMap.has(complement)){
+      console.log({complement})
+      return[i, nums.indexOf(complement)]
     }
+    //if not, add that number to the map for future testing for matches
+    myMap.set(nums[i])
+  }
 }
 
-
-console.log(twoSum([3,3], 6))
-
+twoSum([3,3],6)
