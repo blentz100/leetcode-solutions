6bc2d3c473f28f8c9fc02d4063fbc8664f7ac2a0
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-03-17 13:18:31 -0700

    Update two-sum2.js

--- two-sum2.js
+++ two-sum2.js
@@ -1,3 +1,13 @@
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -54,4 +64,34 @@ while(numbers[left] + numbers[right] !== target){
   }
 }
   return [left +1, right +1]    
+};
+
+
+/*
+Runtime: 1ms, faster than 88.32% of JavaScript online submissions for Two Sum.
+Memory Usage: 55.60 MB, less than 31.57% of JavaScript online submissions for Two Sum.
+*/
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    let currSum;
+    
+    for (let i = 0 ; i <= numbers.length; i++){
+        currSum = numbers[left] + numbers[right]
+        if ( currSum == target){
+            return[left + 1, right + 1]
+        }
+        else if (currSum < target){
+            left++
+        }
+        else {
+            right--
+        }
+    }   
 };
