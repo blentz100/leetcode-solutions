57e8d6cff5cd950996429a149c10d300b3c239e2
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-03-20 10:26:19 -0700

    Update container-with-most-water.js

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -1,1 +1,17 @@
+// https://leetcode.com/problems/container-with-most-water/description/
 
+
+//Brute Force Method (times out on larger inputs)
+
+
+var maxArea = function(height) {
+  let result = 0;
+    
+  for(let i = 0; i < height.length - 1; i++){
+    for(let j = i + 1; j < height.length; j++){
+      //console.log('i is: ', i, ' j is: ', j)
+      result = Math.max(result, (Math.min(height[i], height[j]) * (j - i)))
+    }
+  }
+  return result
+};
