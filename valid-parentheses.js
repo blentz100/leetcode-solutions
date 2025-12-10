8601929cfe64ba548f167087b7157d4b49745042
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-12-10 15:40:07 -0700

    Add isValid function for parentheses validation
    Implement function to check for valid parentheses in a string.

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -10,6 +10,47 @@ Open brackets must be closed in the correct order.
 
 
 
+// 12/10/2025 Solution:
+var isValid = function(s) {
+    // If I see a closing symbol, it has to match the most recent opening symbol
+    const openList = []
+    const symbolArray = s.split("");
+
+    for(let i = 0; i < symbolArray.length; i++) {
+        console.log('openList is: ', openList)
+        if(symbolArray[i] === '(' || symbolArray[i] === '{' || symbolArray[i] === '['){
+            openList.push(symbolArray[i])
+        }
+        else{ 
+            switch (symbolArray[i]){
+                case '}':
+                    if(openList.pop() !== '{') {
+                        return false
+                    }
+                    break;
+                case ')':
+                    if(openList.pop() !== '(') {
+                        return false
+                    }
+                    break;
+                case ']':
+                    if(openList.pop() !== '['){
+                        return false
+                    } 
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    if(openList.length === 0) return true;
+    else return false;
+
+
+
+
+
+
 
 /**
  * @param {string} s
