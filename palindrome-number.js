68af0aabeb692c2255025292393c767acb2fde93
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-12-11 14:02:35 -0700

    Refactor isPalindrome function for efficiency
    Refactor palindrome check logic and improve performance.

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,5 +1,20 @@
 // https://leetcode.com/problems/palindrome-number
 
+// 12/11/2025
+// 12ms beats 22%
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let inputStr = x.toString().split("");
+    for(let x = 0, y = inputStr.length -1; x <= y; x++, y--){
+        if(inputStr[x] !== inputStr[y]) return false
+    }
+    return true;
+};
+
+
 /**
  * @param {number} x
  * @return {boolean}
