a0692c88782d449a1017a1b269320685e252b98c
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-12-11 13:48:01 -0700

    Add maxProfit function for stock trading problem
    Implement maxProfit function to calculate maximum profit from stock prices.

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -1,5 +1,48 @@
-
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+// 12/11/2025
+// 1ms beats 96%
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function(prices) {
+    let left = 0;
+    let right = 1;
+    let workingProfit = 0;
+    let profit = 0;
+    
+    while (right < prices.length){
+    
+       // Profitable Transaction
+       if(prices[left] < prices[right]){
+        // calculate profit
+        workingProfit = prices[right] - prices[left]
+       }
+       // Advance left
+       else {
+        left = right;
+       }
+       // Always advance the right
+       right++
+    
+        if(workingProfit > profit){
+            profit = workingProfit;
+        }
+    }
+    return profit
+};
+
+
+
+
+
+
+
+
+
 // in progress, almost there!
 
 /**
