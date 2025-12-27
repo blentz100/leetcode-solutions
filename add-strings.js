d4739b9e39024adeb5d42767ab273a2291e5ad09
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2025-12-27 15:35:38 -0500

    Implement addStrings function to sum two strings

---
+++ add-strings.js
@@ -0,0 +1,35 @@
+// Add Strings
+// https://leetcode.com/problems/add-strings/
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length -1
+    let j = num2.length -1
+    let carry = 0
+    let runningTotal = "";
+    let workingTotal = 0;
+
+    for(let i = num1.length -1, j = num2.length -1; i >= 0 || j >= 0; i--, j-- ){        
+        workingTotal = (((num1[i] || '0').charCodeAt() - 48) + ((num2[j] || '0').charCodeAt() - 48)) + carry
+        carry = 0;
+        if(workingTotal > 9){
+            carry = 1;
+            runningTotal = runningTotal.concat((workingTotal - 10).toString())
+        }
+        else{
+            runningTotal = runningTotal.concat(workingTotal.toString())
+        }
+    }
+
+    if(carry > 0){
+        return "1" + runningTotal.split("").reverse().join("");
+    }
+    
+    return runningTotal.split("").reverse().join("");
+    
+};
+
