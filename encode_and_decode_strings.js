8528c698912a98701a56cf0197d68c63cb40634b
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2026-01-12 16:23:09 -0700

    Refactor encode and decode methods in Solution class
    Refactor encode and decode methods for better readability and performance.

--- encode_and_decode_strings.js
+++ encode_and_decode_strings.js
@@ -1,3 +1,47 @@
+// 1/12/2016
+// Cleaner Solution
+// .756ms and 53MB memory on Neetcode
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let solution = "";
+        strs.map(token => {
+            solution += `${token.length}©${token}`
+        })
+        console.log('about to return solution: ', solution)
+        return solution
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let solution = [];
+        let tokenLengthString = ""
+        let tokenLengthNumber = 0;
+        let currentToken = ""
+        let i = 0;
+        while( i < str.length){
+            tokenLengthNumber = Number(str.slice(i,str.indexOf('©', i)))
+            i = str.indexOf('©', i) + 1
+            currentToken = str.slice(i,i + tokenLengthNumber)
+            i = i + tokenLengthNumber - 1
+            solution.push(currentToken)
+            i++
+            // reset currentToken and tokenLengthString
+            currentToken = ""
+            tokenLengthString = ""
+        }
+        return solution
+    }
+}
+
+
+
 class Solution {
     /**
      * @param {string[]} strs
