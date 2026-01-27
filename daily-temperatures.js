7469b26dbb74f9892e8dec54f50b8f7e15c18871
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2026-01-27 11:25:20 -0700

    Add dailyTemperatures function to calculate wait days
    Implemented a solution to calculate daily temperatures using a stack to track indices and temperatures.

--- daily-temperatures.js
+++ daily-temperatures.js
@@ -1,3 +1,38 @@
+// Memory: 10.7 MB
+// •
+// Time: 23ms
+// •
+// Submitted at: 01/27/2026 11:24
+
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const result = new Array(temperatures.length).fill(0)
+
+       // The key to this problem is to store pairs in the stack.
+       // The pair contains the temperatures and it's corresponding index from the original input array
+       // We need to keep track of that index somehow, and this is the method. 
+        
+        const stack = [] // pair: [temp, index]
+
+  
+        for(const [index, temp] of temperatures.entries()){
+            while (stack.length !== 0 && temp > stack.at(-1)[0]){
+                let [stackTemp, stackIndex] = stack.pop()
+                result[stackIndex] = index - stackIndex;
+            }
+            stack.push([temp, index])
+        }
+        
+        return result
+    }
+}
+
+
+
 // Brute Force Solution
 
 // Memory: 13.1 MB
